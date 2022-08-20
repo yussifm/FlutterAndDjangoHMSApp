2c0a1cd999ea0e7e2b4ae0eca96ea425d5e819ae
yussifm13 <yussifm13@gmail.com>
2022-08-20 11:22:49 +0000

    backend url

--- backend/blackhathms/api/urls.py
+++ backend/blackhathms/api/urls.py
@@ -3,10 +3,20 @@ from . import views
 
 
 urlpatterns = [
+    # -------Paths for the API-------
     path("patience/", view=views.getpatience),
     path("patience/<str:pk>/", view=views.get_a_patience),
+    path("patience/create/", view=views.create_a_patience),
+    path("patience/update/<str:pk>/", view=views.update_patience),
+    path("patience/delete/<str:pk>/", view=views.delete_patience),
+    # --------------------Doctor------------------------------
     path("doctor/", view=views.get_doctor),
     path("doctor/<str:pk>/", view=views.get_a_doctor),
+    path("doctor/create/", view=views.create_doctor),
+    path("doctor/update/<str:pk>/", view=views.update_doctor),
+    path("doctor/delete/<str:pk>/", view=views.delete_doctor),
+    
+    # --------------------Appointment------------------------------
     path("appointment/", view=views.getAppointments),
     path("appointment/<str:pk>/", view=views.get_an_Appointment),
     path("appointment/create/", view=views.Create_an_Appointment),
